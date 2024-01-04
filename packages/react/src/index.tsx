9b35a8455f92b39f6d3fe690b27269266b1179ef
Matheus Magno <matheusmagnon@gmail.com>
2024-01-04 20:32:54 -0300

    add stiches and configure "on Save" to use eslint config

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,11 @@
-import { colors } from '@front-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$green500',
+  borderRadius: '$md',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.green300 }}>Hello World</h1>
+  return <Button>Hello World</Button>
 }
