9d1f696acbcb509c57d3f9baea6c41bd490e7f99
Matheus Magno <matheusmagnon@gmail.com>
2024-01-09 22:30:19 -0300

    docs: chance box text to component Text

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,16 +1,12 @@
 // tipagens ts
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, BoxProps } from '@front-ui/react'
+import { Box, BoxProps, Text } from '@front-ui/react'
 
 export default {
   title: 'Surfaces/Box',
   component: Box,
   args: {
-    children: (
-      <>
-        <span>Testando o elemento Box</span>
-      </>
-    ),
+    children: <Text>Testando o elemento Box</Text>,
   },
 } as Meta<BoxProps>
 
