66209ab43fe72128860850852d3d3d0e8794deda
Matheus Magno <matheusmagnon@gmail.com>
2024-01-10 21:23:08 -0300

    docs: set dark mode in background components

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -4,6 +4,9 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: "dark",
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
