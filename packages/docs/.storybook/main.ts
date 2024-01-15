8bbce0190ac77597ba5adc81cad10d5437de1f20
Matheus Magno <matheusmagnon@gmail.com>
2024-01-15 19:33:22 -0300

    docs: install addon accessibility

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -16,7 +16,9 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-backgrounds")
+    getAbsolutePath("@storybook/addon-backgrounds"),
+    getAbsolutePath("@storybook/addon-a11y")
+
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
